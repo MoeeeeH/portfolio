c59a3d311486d2255134f5bd7272cdb4461e846b
Mohamed Hassen <mohamedh120802@gmail.com>
2025-09-13 19:37:06 +0100

    test

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',       // export static HTML
-  images: { unoptimized: true }, // GitHub Pages doesn't handle Next.js image optimization
-  basePath: '/portfolio', // ⚠️ replace with your repo name
-  assetPrefix: '/portfolio/',
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
